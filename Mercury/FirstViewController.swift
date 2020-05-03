22b8566b367366046899188337fa7f2b65f63288
Andrey <stig1467@gmail.com>
2020-05-03 20:55:20 +0300

    Initial Commit

---
+++ Mercury/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Mercury
+//
+//  Created by Andrey  Grechko on 03.05.2020.
+//  Copyright © 2020 Andrey Grechko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
